0d96e3d46409933406c7fcfc2ca503a1a4967400
mariesaoconnell <mariesa.oconnell@gmail.com>
2022-09-12 12:59:06 -0400

    MVP completed

--- js/start.js
+++ js/start.js
@@ -1,63 +1,235 @@
+// START MENU JS
+
+// ==== VARIABLES ====
+
+const mainContainer = document.querySelector('#mainContainer');
+
+const gameContainer = document.querySelector('#game');
+
+const gameStats = document.querySelector('#stats');
+
+const charImg = document.querySelector('#characterImg');
+
+
 let characterChoice = document.querySelector('#characterChoice');
 console.log(characterChoice);
 
-// ==== ON START ====
+let playerCharacter = null;
+
+// ==== 🟢 ON START ====
 function load() {
 	ogNyan();
+	updateCharacter();
 }
-
 load();
 
-// ==== CHARACTER CHANGE FUNCS ====
+// ==== 🟢 CLICKED START FUNCTION ====
+function clickedStart(){
+	mainContainer.style.visibility='hidden';
+	gameContainer.style.visibility='visible';
+	gameStats.style.visibility='visible';
+
+	setCharacter();
+}
+
+
+// ==== 🟢 CHARACTER CHANGE FUNCS ====
 
 function ogNyan() {
 	characterChoice.src = '/assets/OriginalNyanCat.webp';
+	updateCharacter();
 }
 
 function pirateNyan() {
 	characterChoice.src = '/assets/PirateNyanCat.webp';
+	updateCharacter();
 }
 
 function mummyNyan() {
 	characterChoice.src = '/assets/Mummy-2-.webp';
+	updateCharacter();
 }
 
 function rastaNyan() {
 	characterChoice.src = '/assets/Rasta.webp';
+	updateCharacter();
 }
 
 function gbNyan() {
 	characterChoice.src = '/assets/Gb.webp';
+	updateCharacter();
 }
 
 function witchyNyan() {
 	characterChoice.src = '/assets/Magical.webp';
+	updateCharacter();
 }
 
 function tacoNyan() {
 	characterChoice.src = '/assets/Mexinyan.webp';
+	updateCharacter();
 }
 
 function pumpkinNyan() {
 	characterChoice.src = '/assets/Pumpkin.webp';
+	updateCharacter();
 }
 
 function smurfNyan() {
 	characterChoice.src = '/assets/Smurf.webp';
+	updateCharacter();
 }
 
 function daftNyan() {
 	characterChoice.src = '/assets/Dafty.webp';
+	updateCharacter();
 }
 
 function pusheenNyan() {
 	characterChoice.src = '/assets/Fat.webp';
+	updateCharacter();
 }
 
 function toasterNyan() {
 	characterChoice.src = '/assets/SonOfaNyan.webp';
+	updateCharacter();
 }
 
 function partyNyan() {
 	characterChoice.src = '/assets/BdayFlavor.webp';
+	updateCharacter();
+}
+
+function updateCharacter() {
+	playerCharacter = characterChoice;
+	console.log(playerCharacter);
+}
+
+
+
+
+
+// ==== 🟢 GAME JAVA SCRIPT ====
+
+
+
+
+// ==== VARIABLES ====
+
+const block = document.querySelector('#block');
+const hole = document.querySelector('#hole');
+const character = document.querySelector('#character');
+
+const body = document.querySelector('body');
+
+let scoreSpan = document.querySelector('#score');
+
+let jumping = 0;
+let score = 0;
+
+// ==== EVENT LISTENERS ====
+body.addEventListener('keypress', () => {
+	startGame();
+});
+
+// 🟢 EVENT LISTENER -- changes positioning of hole
+
+hole.addEventListener('animationiteration', () => {
+	let random = -(Math.random() * 700 + 300);
+	hole.style.top = `${random}px`;
+
+	score++;
+	scoreUpdate();
+});
+
+// ==== FUNCTIONS ====
+
+function startGame() {
+	setInterval(gravityFunc, 10); // gravity starts
+
+	// block & hole animation toggle
+	hole.style.animationPlayState = 'running';
+	block.style.animationPlayState = 'running';
+}
+
+// 🟢 SCORE FUNC
+function scoreUpdate() {
+	scoreSpan.innerHTML = score;
+}
+
+// 🟢 GRAVITY FUNC
+function gravityFunc() {
+	let characterTop = parseInt(
+		window.getComputedStyle(character).getPropertyValue('top')
+	);
+
+	// 🟢 GRAVITY EFFECT
+	if (jumping === 0) {
+		character.style.top = characterTop + 3 + 'px';
+	}
+
+	let blockLeft = parseInt(
+		window.getComputedStyle(block).getPropertyValue('left')
+	);
+	let holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue('top'));
+
+	// height of game - characterTop
+	let cTop = -(1100 - characterTop);
+
+	// 🟢 VERIFYING GAME OVER CONDITIONS
+	if (
+		characterTop >= 1025 ||
+		(blockLeft < 100 &&
+			blockLeft > -50 &&
+			(cTop < holeTop || cTop > holeTop + 270))
+	) {
+		alert(`Game Over. Score: ${score}`);
+		character.style.top = 500 + 'px';
+		score = 0;
+	}
+}
+
+// 🟢 JUMP FUNC
+function jump() {
+	jumping = 1;
+
+	let jumpCount = 0;
+
+	let jumpInterval = setInterval(function () {
+		let characterTop = parseInt(
+			window.getComputedStyle(character).getPropertyValue('top')
+		);
+
+		character.style.top = characterTop - 5 + 'px';
+
+		if (characterTop <= 125) {
+			characterTop = 125;
+			character.style.top = 125 + 'px';
+		}
+
+		if (character > 6 && counter < 15) {
+			character.style.top = characterTop - 5 + 'px';
+		}
+
+		if (jumpCount > 20) {
+			clearInterval(jumpInterval);
+			jumping = 0;
+		}
+
+		jumpCount++;
+	}, 10);
+}
+
+
+// ==== 🟢 SET CHAR FUNC ====
+function setCharacter() {
+	charImg.src = playerCharacter.src;
+	console.log(charImg);
+}
+
+
+// ==== EXIT / RELOAD ====
+
+function reload(){
+	window.location.reload();
 }
